459db5fb68c3401556c4cd760f71c3e7ab69f3bc
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2019-11-20 15:36:31 -0500

    Final Commit, the project is already been made

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+	public Rigidbody2D Rb2D;
+	private int scoreOne = 0;
+	private int scoreTwo = 0;
+	private bool GameEnded = false;
+	public Text TscoreOne;
+	public Text TscoreTwo;
+	public Text Tresult;
+
+	void Awake()
+	{
+		Rb2D.GetComponent<Rigidbody2D>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		ForceBall();
+		TscoreOne.text = scoreOne.ToString();
+		TscoreTwo.text = scoreTwo.ToString();
+		Tresult.text = "";
+	}
+	
+	void Update()
+	{
+		if (GameEnded)
+		{
+			if (Input.GetKey(KeyCode.Space))
+			{
+				RestartGame();
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		UpdateScore(other.name);
+		if (scoreOne == 11)
+		{
+			Tresult.text = "PLAYER ONE WINS";
+			Rb2D.velocity = Vector2.zero;
+			GameEnded = true;
+		}
+
+		if (scoreTwo == 11)
+		{
+			Tresult.text = "PLAYER TWO WINS";
+			Rb2D.velocity = Vector2.zero;
+			GameEnded = true;
+		}
+		Rb2D.MovePosition(new Vector2(0, 0));
+		//ForceBall();
+		
+	}
+
+	void ForceBall()
+	{
+		Rb2D.velocity = new Vector2(6, 2);
+	}
+
+	void UpdateScore(string ColliderName)
+	{
+		if (ColliderName == "GoalForTwo")
+		{
+			scoreOne++;
+			TscoreOne.text = scoreOne.ToString();
+		}
+		else
+		{
+			scoreTwo++;
+			TscoreTwo.text = scoreTwo.ToString();
+		}
+	}
+
+	void RestartGame()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			scoreOne = 0;
+			scoreTwo = 0;
+			Start();
+		}
+	}
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+	[SerializeField] private int player;
+	private KeyCode up;
+	private KeyCode down;
+
+	private Rigidbody2D myBody;
+	// Start is called before the first frame update
+	void Awake()
+	{
+		if (player == 1)
+		{
+			up = KeyCode.W;
+			down = KeyCode.S;
+		}
+		else
+		{
+			up = KeyCode.UpArrow;
+			down = KeyCode.DownArrow;
+		}
+
+		myBody = GetComponent<Rigidbody2D>();
+	}
+
+  // Update is called once per frame
+  void Update()
+  {
+	  if (Input.GetKey(up))
+	  {
+		  myBody.velocity = new Vector2(0, 10.0f);
+	  }
+	  if (Input.GetKeyUp(up))
+	  {
+		  myBody.velocity = Vector2.zero;
+	  }
+
+		if (Input.GetKey(down))
+	  {
+			myBody.velocity = new Vector2(0, -10.0f);
+	  }
+
+		if (Input.GetKeyUp(down))
+		{
+			myBody.velocity = Vector2.zero;
+		}
+
+  }
+}
